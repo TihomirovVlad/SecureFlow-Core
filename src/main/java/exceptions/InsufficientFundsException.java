89c431e8857cc785a87ea764c435e57f42475832
yotsume <yotsume52@gmail.com>
2025-10-07 00:25:30 +0300

    add exception for AccountRepository

---
+++ src/main/java/exceptions/InsufficientFundsException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class InsufficientFundsException extends  RuntimeException{
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+    public InsufficientFundsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
