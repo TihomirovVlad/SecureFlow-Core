8b73cf35756970e01d82c2b02f8b123767f124fd
yotsume <yotsume52@gmail.com>
2025-10-08 01:39:38 +0300

    fix OperationLogRepository and OperationLogRepositoryImpl

--- src/main/java/repository/OperationLogRepository.java
+++ src/main/java/repository/OperationLogRepository.java
@@ -4,8 +4,13 @@ import model.OperationLog;
 import model.OperationType;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 public interface OperationLogRepository {
     OperationLog save(OperationType operationType, Long fromAccountId, Long toAccountId,
               BigDecimal amount, BigDecimal commission, Long userId);
+
+    Optional<OperationLog> findByOperationLogId(Long OperationLogId);
+    List<OperationLog> findByUserId(Long userId);
 }
--- src/main/java/repository/OperationLogRepositoryImpl.java
+++ src/main/java/repository/OperationLogRepositoryImpl.java
@@ -2,18 +2,19 @@ package repository;
 
 import model.OperationLog;
 import model.OperationType;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class OperationLogRepositoryImpl implements OperationLogRepository {
@@ -93,5 +94,36 @@ public class OperationLogRepositoryImpl implements OperationLogRepository {
         operationLog.setCreatedAt(LocalDateTime.now());
         operationLog.setUserId(userId);
         return operationLog;
+    }
+
+    @Override
+    public Optional<OperationLog> findByOperationLogId(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("OperationLogId cannot be null");
+        }
+        String sql = "SELECT * FROM operations_log WHERE id = :id";
+        Map<String, Object> params = Map.of("id", id);
+        try {
+            OperationLog operationLog = namedParameterJdbcTemplate.queryForObject(sql, params, ROW_MAPPER);
+            return Optional.ofNullable(operationLog);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<OperationLog> findByUserId(Long userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException("UserId cannot be null");
+        }
+        return namedParameterJdbcTemplate
+                .query("""
+                            SELECT id, operation_type, from_account_id, to_account_id, amount, commission, created_at, user_id
+                            FROM operations_log
+                            WHERE user_id = :userId
+                            ORDER BY created_at DESC
+                            """,
+                        Map.of("userId", userId),
+                        ROW_MAPPER);
     }
 }
